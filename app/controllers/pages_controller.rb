094ea1cd890acaf13fc9ae83b0c2b54ccaa8922a
Eric Hayes <eric@erichayes.net>
2012-05-21 08:54:52 -0700

    Helper for main nav

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
+  
+  before_filter :load_root_nav
+  
   def index
     @page = Page.home_page
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  
+  def main_nav(pages, admin_mode = false)
+    pages.each do |page|
+      content_tag :li do
+        link_to page.title, switch_path(page, admin_mode)
+      end
+    end
+  end
+  
+  def switch_path(resource, admin_mode = false)
+    if admin_mode
+      path = admin_page_path(resource)
+    else
+      path = page_path(resource)
+    end
+    
+  end
+  
 end
