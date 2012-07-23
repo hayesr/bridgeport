a5bcc1d07a6abdc4388be04df2d379046b7487e5
Eric Hayes <eric@erichayes.net>
2012-07-23 09:13:50 -0700

    Precompile please

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -24,9 +24,9 @@ namespace :deploy do
   
 end
 
-# after 'deploy:update_code' do
-#   run "cd #{release_path}; RAILS_ENV=production bundle exec rake assets:precompile"
-# end
+after 'deploy:update_code' do
+  run "cd #{release_path}; RAILS_ENV=production bundle exec rake assets:precompile"
+end
 
 after "deploy:update_code", "deploy:uploads_link"
 after "deploy:update_code", "deploy:ownership"
