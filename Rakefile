a1a184e2fd26fa9a7ddb896d2b5805729706e2d5
Eric Hayes <eric@erichayes.net>
2012-07-30 16:20:58 -0700

    Remaining name changes

--- Rakefile
+++ Rakefile
@@ -4,4 +4,4 @@
 
 require File.expand_path('../config/application', __FILE__)
 
-Schoolcms::Application.load_tasks
+Bridgeport::Application.load_tasks
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,5 +5,5 @@ Bridgeport::Application.config.session_store :cookie_store, key: '_bridgeport_se
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# Schoolcms::Application.config.session_store :active_record_store
+# Bridgeport::Application.config.session_store :active_record_store
 Bridgeport::Application.config.session_store :mongoid_store
